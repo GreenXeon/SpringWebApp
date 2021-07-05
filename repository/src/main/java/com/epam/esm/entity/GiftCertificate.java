11d189afee6da7cd1f4fe575828600ecef9ade89
Zakhar Shyshkin <zahar.shishkin.2001@mail.ru>
2021-07-05 16:51:10 +0300

    builder pattern

--- repository/src/main/java/com/epam/esm/entity/GiftCertificate.java
+++ repository/src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -67,4 +67,79 @@ public class GiftCertificate {
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GiftCertificate that = (GiftCertificate) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (price != null ? !price.equals(that.price) : that.price != null) return false;
+        if (duration != null ? !duration.equals(that.duration) : that.duration != null) return false;
+        if (createDate != null ? !createDate.equals(that.createDate) : that.createDate != null) return false;
+        return lastUpdateDate != null ? lastUpdateDate.equals(that.lastUpdateDate) : that.lastUpdateDate == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (duration != null ? duration.hashCode() : 0);
+        result = 31 * result + (createDate != null ? createDate.hashCode() : 0);
+        result = 31 * result + (lastUpdateDate != null ? lastUpdateDate.hashCode() : 0);
+        return result;
+    }
+
+    public static class Builder{
+        private GiftCertificate giftCertificate;
+
+        public Builder(){
+            this.giftCertificate = new GiftCertificate();
+        }
+
+        public Builder withId(Long id){
+            giftCertificate.id = id;
+            return this;
+        }
+
+        public Builder withName(String name){
+            giftCertificate.name = name;
+            return this;
+        }
+
+        public Builder withDescription(String description){
+            giftCertificate.description = description;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal price){
+            giftCertificate.price = price;
+            return this;
+        }
+
+        public Builder withDuration(Integer duration){
+            giftCertificate.duration = duration;
+            return this;
+        }
+
+        public Builder withCreateDate(LocalDateTime createDate){
+            giftCertificate.createDate = createDate;
+            return this;
+        }
+
+        public Builder withLastUpdateDate(LocalDateTime lastUpdateDate){
+            giftCertificate.lastUpdateDate = lastUpdateDate;
+            return this;
+        }
+
+        public GiftCertificate build(){
+            return giftCertificate;
+        }
+    }
 }
--- repository/src/main/java/com/epam/esm/entity/Tag.java
+++ repository/src/main/java/com/epam/esm/entity/Tag.java
@@ -19,4 +19,44 @@ public class Tag {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        if (id != null ? !id.equals(tag.id) : tag.id != null) return false;
+        return name != null ? name.equals(tag.name) : tag.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    public static class Builder{
+        private Tag tag;
+
+        public Builder() {
+            this.tag = new Tag();
+        }
+
+        public Builder withId(Long id){
+            tag.id = id;
+            return this;
+        }
+
+        public Builder withName(String name){
+            tag.name = name;
+            return this;
+        }
+
+        public Tag build(){
+            return tag;
+        }
+    }
 }
