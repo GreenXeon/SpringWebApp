60f4b680acd99923825b29825a122fa1e99e0d29
Zakhar Shyshkin <zahar.shishkin.2001@mail.ru>
2021-07-09 18:15:23 +0300

    added seconds and system default timezone for LocalDateTime

--- service/src/main/java/com/epam/esm/util/ServiceUtils.java
+++ service/src/main/java/com/epam/esm/util/ServiceUtils.java
@@ -3,6 +3,7 @@ package com.epam.esm.util;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.TimeZone;
@@ -12,8 +13,8 @@ public class ServiceUtils {
      * @return current date in iso 8601 format
      */
     public static LocalDateTime getCurrentDateTime() {
-        TimeZone tz = TimeZone.getTimeZone("UTC");
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        TimeZone tz = TimeZone.getTimeZone(ZoneId.systemDefault());
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         df.setTimeZone(tz);
         return LocalDateTime.from(ZonedDateTime.parse(df.format(new Date())));
     }
